1e6fe3cfe5a04ba5f3d03f0ac99043f6220839da
Seth <itz.saga@gmail.com>
2018-02-11 20:40:58 -0600

    Custom HTML response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,10 @@
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+    new Response("<div class='a-winner-is-me'>Intercepted!</div>", {
+      headers: {'Content-Type': 'text/html'}
+    })
+  );
 });
