e5f57de360bc56db9406a133d43d071e2d96963b
Seth <itz.saga@gmail.com>
2018-02-11 21:50:55 -0600

    The scalable version

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,11 @@
-self.addEventListener('install', function(event) {
+var staticCacheName = 'wittr-static-v2';
+
+self.addEventListener('install', event => {
   event.waitUntil(
     // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
     // Ensure at least $primary-color changes
     // TODO: change cache name to 'wittr-static-v2'
-    caches.open('wittr-static-v2').then(function(cache) {
+    caches.open(staticCacheName).then(cache => {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -16,15 +18,24 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.delete('wittr-static-v1')
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('wittr-') &&
+                 cacheName != staticCacheName;
+        }).map(cacheName => {
+          return cache.delete(cacheName);
+        })
+      );
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
